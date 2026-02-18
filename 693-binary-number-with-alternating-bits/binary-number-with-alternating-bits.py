3e3ac25501ebc584837eadb8addffd8c56acd0f0
Kairamkonda varshini <kairamkondavarshini13@gmail.com>
2026-02-18 19:40:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (50.00%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        x = n ^ (n >> 1)   # XOR with shifted version
+        x = n ^ (n >> 1)   
         return (x & (x + 1)) == 0
 
         
